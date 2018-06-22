1a09b1ba58574eb5aeced9b70d29cf8eacc03d5f
Min-Hsuan Hung <poxuan33@gmail.com>
2018-06-22 15:10:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function happyHolidaysTo(name) {return`Happy holidays, ${name}!`};
 
 function happyHolidayTo(holiday, name) {return`Happy ${holiday}, ${name}!`};
 
-function holidayCountdown(number, holiday) {return `It's ${number} days until ${holiday}!`};
+function holidayCountdown(number, holiday) {return `It's ${holiday} days until ${number}!`};
 holidayCountdown ("20","Mother's Day!")
