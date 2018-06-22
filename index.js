c313f61c3585102828a0070bf5d74958f104572f
Min-Hsuan Hung <poxuan33@gmail.com>
2018-06-22 14:34:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 // write your code below!
-function add(a, b) {return a * b };
-
-function sayHi() {console.log("Hi!")};
-
-function doNotGreetMe(firstName) {console.log(`No,${firstName}!`)} 
+function happyHolidays () console.log ("Happy holldays!")
+ 
